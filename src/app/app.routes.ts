8f7887288afb6c03973feb5db37178bee239b94d
a.javierleonc <a.javierleonc@gmail.com>
2024-08-20 21:23:13 +0000

    Se mejoran aspectos de validaciones y se crea paginalegal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LegalComponent } from './pages/legal/legal.component';
 
 export const routes: Routes = [
     { path: 'home', component: LoginComponent },
     { path: '', redirectTo : 'home', pathMatch : 'full' },
+    {path: 'policity', component: LegalComponent},
     {path: 'inicio', component: DashboardComponent}
 ];
